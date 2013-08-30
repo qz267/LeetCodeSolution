e23dfa84de7856c58a9decc91bd59db7285d8a44
Zheng Qin <kevin.qin.cn@gmail.com>
2013-08-30 00:20:26 -0400

    Add longest palindromic substring solution in cpp

---
+++ CPP/LongestPalindromicSubstring.cpp
@@ -0,0 +1,27 @@
+/*
+ Notes:
+ Given a string S, find the longest palindromic substring in S. You may assume
+ that the maximum length of S is 1000, and there exists one unique longest
+ palindromic substring.
+
+ Solution:
+ Find the longest substring of s and the reversion of s. And find the longest
+ common substring with dynamic programmming (O(n^2)). For the longest common substring
+ problem, it can also be solved by suffix tree (O(2n)).
+ */
+#include <vector>
+#include <set>
+#include <climits>
+#include <algorithm>
+#include <iostream>
+#include <sstream>
+#include <cmath>
+#include <cstring>
+using namespace std;
+
+class Solution {
+public:
+	string longestPalindrome(string s) {
+
+	}
+};
