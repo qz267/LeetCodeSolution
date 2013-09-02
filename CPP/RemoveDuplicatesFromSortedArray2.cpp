60717f641d6d3fc52e6acd6217585af1dcc290e1
Zheng Qin <kevin.qin.cn@gmail.com>
2013-09-01 22:56:48 -0400

    Add remove duplicates from sorted array 2 solution in cpp

---
+++ CPP/RemoveDuplicatesFromSortedArray2.cpp
@@ -0,0 +1,37 @@
+/*
+ Notes:
+ Follow up for "Remove Duplicates":
+ What if duplicates are allowed at most twice?
+
+ For example,
+ Given sorted array A = [1,1,1,2,2,3],
+
+ Your function should return length = 5, and A is now [1,1,2,2,3].
+ Solution:
+
+ */
+#include <vector>
+#include <set>
+#include <climits>
+#include <algorithm>
+#include <iostream>
+#include <sstream>
+#include <cmath>
+#include <cstring>
+using namespace std;
+
+class Solution {
+public:
+	int removeDuplicates(int A[], int n) {
+		int len = 0;
+		for (int i = 0, count = 0, k = 0; i < n; i++) {
+			count = (i == 0 || A[i] == A[i - 1] ? count + 1 : 1);
+			if (count <= 2) {
+				A[k] = A[i];
+				len++;
+				k++;
+			}
+		}
+		return len;
+	}
+};
