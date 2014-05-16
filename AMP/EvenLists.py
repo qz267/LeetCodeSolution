1a917bde3ca0d3f060c5b030bec666a6c7395228
Zheng Qin <kevin.qin.cn@gmail.com>
2014-05-16 00:31:45 -0400

    add solution even lists

--- AMP/EvenLists.py
+++ AMP/EvenLists.py
@@ -13,3 +13,28 @@ class Solution(object):
     input {50,10,200,150,900,250,40,50,950,5,90,80,60,600}
     List 1: 900, 600, 90, 60, 50, 10, 5, List 2: 950, 250, 200, 150, 80, 50, 40,
     """
+
+    def even_list(self, list):
+        temp = list
+        l = len(temp)
+        temp.sort()
+        l1, l2 = [], []
+        count1, count2 = 0, 0
+        for i in temp:
+            if count1 < count2 and len(l1) < l/2:
+                l1.append(i)
+                count1 += i
+            else:
+                l2.append(i)
+                count2 += i
+
+        result = []
+        result.append(l1)
+        result.append(l2)
+        return result
+
+
+l = [1000,500,200,1,5,10,50,70,70,100]
+
+s = Solution()
+print(s.even_list(l))
