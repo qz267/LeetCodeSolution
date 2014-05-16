784031423ad24190e01df8063e72a984ff020bd0
Zheng Qin <kevin.qin.cn@gmail.com>
2014-05-15 23:53:41 -0400

    add solution array intersection

--- AMP/ArrayIntersection.py
+++ AMP/ArrayIntersection.py
@@ -5,3 +5,13 @@ class Solution(object):
     """
     Question: Find the intersection of two unsorted integer arrays.
     """
+
+    def array_intersection(self, s1, s2):
+        return list(set(s1) & set(s2))
+
+
+s1 = [1,2,2,2,3,4,5,63,4,56]
+s2 = [2,3,4]
+
+s = Solution()
+print s.array_intersection(s1, s2)
