2720aebbb7e84f1d82dff2b9a37240061112c53e
Zheng Qin <kevin.qin.cn@gmail.com>
2014-05-15 23:43:37 -0400

    add anagrams solution

--- AMP/Anagrams.py
+++ AMP/Anagrams.py
@@ -5,3 +5,15 @@ class Solution(object):
     """
     Question: Write an algorithm to determine whether two strings are anagrams or not.
     """
+
+    def is_anagrams(self, s1, s2):
+        if len(s1) != len(s2):
+            return False
+        else:
+            return  s2[::-1] == s1
+
+s1 = "123"
+s2 = "3241"
+
+s = Solution()
+print(s.is_anagrams(s1,s2))
