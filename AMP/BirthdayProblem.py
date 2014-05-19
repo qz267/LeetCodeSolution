b6893e560080a456c3b3e003800e7b2417a29e41
Zheng Qin <kevin.qin.cn@gmail.com>
2014-05-18 23:48:49 -0400

    add birthday problem solution

--- AMP/BirthdayProblem.py
+++ AMP/BirthdayProblem.py
@@ -6,3 +6,12 @@ class Solution(object):
     Question: Find the probability that, given n people, a pair of them will have the same birthday.
     (Based off the Birthday Paradox)
     """
+
+    def birthday_problem(self, n):
+        np = 1.0
+        for i in range(n):
+            np *= ((365.0 - float(i))/365.0)
+        return 1 - np
+
+s = Solution()
+print(s.birthday_problem(364))
